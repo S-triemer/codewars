7b4b06373866d3578b9d5bc4109d06eeef852ab6
Sebastian Triemer <sebastian.triemer@outlook.de>
2022-04-05 16:53:07 +0200

    solved rps

---
+++ kyu8/rock-paper-scissors.js
@@ -0,0 +1,16 @@
+//Return which player has won the game
+//if scissor && paper -> scissor
+//if paper %% rock -> paper
+//if rock && scissor -> rock
+
+const rps = (p1, p2) => {
+  if (
+    (p1 == "scissors" && p2 == "paper") ||
+    (p1 == "paper" && p2 == "rock") ||
+    (p1 == "rock" && p2 == "scissors")
+  ) {
+    return "Player 1 won!";
+  } else if (p1 === p2) {
+    return "Draw!";
+  } else return "Player 2 won!";
+};
