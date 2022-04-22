7f1fbb996ef5b448a2776cc08c264d7cd6fdc18e
Sebastian Triemer <sebastian.triemer@outlook.de>
2022-04-22 15:13:50 +0200

    solved array challenge

---
+++ kyu8/remove-first-last-char.js
@@ -0,0 +1,41 @@
+/*
+This is a spin off of my first kata.
+
+You are given a string containing a sequence of character sequences separated by commas.
+
+Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+
+If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+Examples
+
+"1,2,3"      =>  "2"
+"1,2,3,4"    =>  "2 3"
+"1,2,3,4,5"  =>  "2 3 4"
+
+""     =>  NULL
+"1"    =>  NULL
+"1,2"  =>  NULL
+
+*/
+
+function array(str) {
+  //Get Array of String Chars
+  let arr = str.split(",");
+
+  //Remove first and last index of array
+  arr.pop();
+  arr.shift();
+
+  //concat array to a new string with whitespaces between chars.
+  let resultString = arr.join(" ").trim();
+
+  //return the result
+  return str == "" || arr.length == 0 ? null : resultString;
+}
+
+/*
+Clever Solution:
+function array(arr){
+  return arr.split(",").slice(1,-1).join(" ") || null;
+}
+*/
